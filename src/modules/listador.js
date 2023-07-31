08b00935364d4264ca79d88c6f03d7c6a3ebc248
Macflaims <martinsanchez2533@gmail.com>
2023-07-31 08:36:43 -0600

    Crea la lista de pokemons

---
+++ src/modules/listador.js
@@ -0,0 +1,39 @@
+
+import { mostrarTextoTotalPokemones, bloquearPaginador, desbloquearPaginador, comprobarCargaTabla, mostrarCargando } from "./general.js"; 
+import { mostrarPokemon } from "./pokemon.js";
+import { manejarCambioPagina, eliminarPaginasAnteriores } from "./paginador.js";
+
+const POKEMONES_POR_PAG = 10;
+const CANTIDAD_DE_ITEMS_PAGINADOR = 5;
+
+
+function cargarPokedex(url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${POKEMONES_POR_PAG}`, PAGINA_ACTUAL = 1){
+  mostrarCargando();
+  eliminarPaginasAnteriores();
+  bloquearPaginador(); 
+    fetch(url)
+    .then((r)=> r.json())
+    .then((r)=> {
+       let {count: totalPokemones, results: pokemones, next:paginaSiguiente, previous: paginaAnterior}= r;
+       mostrarTextoTotalPokemones(totalPokemones);
+       cargarListadoPokemones(pokemones).then(()=>{
+        comprobarCargaTabla()
+        desbloquearPaginador();
+      })
+       manejarCambioPagina(paginaAnterior, paginaSiguiente, CANTIDAD_DE_ITEMS_PAGINADOR, PAGINA_ACTUAL, POKEMONES_POR_PAG)
+    })
+}
+
+
+async function cargarListadoPokemones(pokemones) {
+    for (const pokemon of pokemones) {
+      const { name: nombre } = pokemon;
+      const r = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+      const pokemonData = await r.json();
+      mostrarPokemon(pokemonData)
+    }
+}
+
+
+
+export {cargarPokedex, POKEMONES_POR_PAG}
